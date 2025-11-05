3de4ac65e335c22e38d742c50b5f47acb59f6bb4
wessam mohamed <chaos5starz@gmail.com>
2025-11-05 22:09:26 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+api_key = os.getenv("API-KEY")
+print("enter a city name")
+city_name = input()
+
+
+url = "https://api.openweathermap.org/data/2.5/weather"
+
+
+params = {
+    "q": city_name,     
+    "appid": api_key,   
+    "units": "metric"   
+}
+
+headers = {
+    "Content-Type": "application/json",  # tells server what kind of data we want
+}
+response = requests.get(url, headers=headers , params=params)
+
+data = response.json()
+
+print(data)
