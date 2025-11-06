ece869584b50fde55f079d05f52c5c731a002f88
wessam mohamed <chaos5starz@gmail.com>
2025-11-06 23:42:44 +0200

    Added error handling and parsed key weather data (temperature, humidity, wind, etc.)

--- main.py
+++ main.py
@@ -6,24 +6,47 @@ import os
 load_dotenv()
 
 api_key = os.getenv("API-KEY")
-print("enter a city name")
-city_name = input()
 
+while True :
 
-url = "https://api.openweathermap.org/data/2.5/weather"
+    print("enter a city name or type exit to exit")
+    city_name = input().lower()
 
+    if city_name == "exit" :
+        print("bye")
+        break
 
-params = {
-    "q": city_name,     
-    "appid": api_key,   
-    "units": "metric"   
-}
+    url = "https://api.openweathermap.org/data/2.5/weather"
 
-headers = {
-    "Content-Type": "application/json", 
-}
-response = requests.get(url, headers=headers , params=params)
+    params = {
+        "q": city_name,     
+        "appid": api_key,   
+        "units": "metric"   
+    }
 
-data = response.json()
+    headers = {
+        "Content-Type": "application/json",
+    }
 
-print(data)
+    try :
+        response = requests.get(url , headers = headers , params=params , timeout = 5)
+        response.raise_for_status()
+        data = response.json()
+
+    except requests.exceptions.HTTPError :
+        print("invalid city name")
+        continue
+
+    except requests.exceptions.Timeout :
+        print("poor connection , try again later")
+        continue
+
+    if data.get("cod") == 200 :
+        print("Temp:", data["main"]["temp"])
+        print("Feels like:", data["main"]["feels_like"])
+        print("Weather:", data["weather"][0]["description"])
+        print("Humidity:", data["main"]["humidity"])
+        print("Wind speed:", data["wind"]["speed"])
+    else :
+        print ("invalid city name")
+        continue
